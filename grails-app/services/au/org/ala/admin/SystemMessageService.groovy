2e240e02dc970a2d0cad19f30b162115cb0cf9fc
Simon Bear <simon.bear@csiro.au>
2017-07-21 14:51:26 +1000

    Fix for app.name metadata property, fix for system message file IO exceptions, use a later Apache HTTP client because we care

--- grails-app/services/au/org/ala/admin/SystemMessageService.groovy
+++ grails-app/services/au/org/ala/admin/SystemMessageService.groovy
@@ -2,6 +2,7 @@ package au.org.ala.admin
 
 import grails.plugin.cache.CacheEvict
 import grails.plugin.cache.Cacheable
+import grails.util.Metadata
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 
@@ -11,7 +12,7 @@ class SystemMessageService {
 
     @CacheEvict(value = ["systemMessageCache"], allEntries = true)
     void setSystemMessage(SystemMessage message) {
-        getSystemMessageFile().write(new JsonBuilder([message: message?.text ? message : [:]]).toPrettyString())
+        getSystemMessageFile()?.write(new JsonBuilder([message: message?.text ? message : [:]])?.toPrettyString()) ?: ''
     }
 
     @Cacheable("systemMessageCache")
@@ -31,16 +32,24 @@ class SystemMessageService {
     }
 
     private File getSystemMessageFile() {
-        File dataDir = new File("/data/${grailsApplication.metadata['app.name']}/config")
-        if (!dataDir.exists()) {
-            dataDir.mkdirs()
+        File systemMsgFile
+        try {
+            def path = grailsApplication.config.ala.admin.systemMessage.path ?: "/data/${Metadata.current.getApplicationName()}/config"
+            File dataDir = new File(path)
+            if (!dataDir.exists()) {
+                dataDir.mkdirs()
+            }
+
+            def fileName = grailsApplication.config.ala.admin.systemMessage.fileName ?: "system-message.json"
+            systemMsgFile = new File(dataDir, fileName)
+            if (!systemMsgFile.exists()) {
+                systemMsgFile.createNewFile()
+            }
+        } catch (IOException e) {
+            log.warn("Could not getSystemMessageFile", e)
+            systemMsgFile = null
         }
 
-        File systemMsgFile = new File(dataDir, "system-message.json")
-        if (!systemMsgFile.exists()) {
-            systemMsgFile.createNewFile()
-        }
-
-        systemMsgFile
+        return systemMsgFile
     }
 }
